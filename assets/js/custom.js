31c3d5e8236d3d885a52700df8721248b65357fd
Maciej <m@m.pl>
2025-06-13 19:16:18 -0400

    fixes for mobile web

--- assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-// assets/js/custom.js
-console.log('[MK DEBUG] custom.js IS RUNNING - TOP LEVEL');
-
-(function($) {
-    console.log('[MK DEBUG] custom.js - IIFE RUNNING');
-    $(document).ready(function() {
-        console.log('[MK DEBUG] custom.js - DOCUMENT READY');
-    });
-})(jQuery);
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-console.log('[MK DEBUG] main.js IS RUNNING - TOP LEVEL');
 
 (function($) {
 
